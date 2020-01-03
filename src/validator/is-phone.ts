3e5dfd47f8083cba5936ecfd7e10eca30c661ff4
halilteyfik <hlltvfk@gmail.com>
2020-01-03 21:42:37 +0300

    update is-phone decorator\'s env locale

--- src/validator/is-phone.ts
+++ src/validator/is-phone.ts
@@ -1,3 +1,3 @@
 import { IsPhoneNumber } from 'class-validator';
 
-export const IsPhone = () => IsPhoneNumber(process.env.PHONE_LOCAL);
+export const IsPhone = () => IsPhoneNumber(process.env.phoneLocal);
